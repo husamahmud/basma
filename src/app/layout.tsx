bc9fabcfee09f08de56a95c262d43f78d91317f2
husamahmud <husamahmud@gmail.com>
2024-09-17 00:15:06 +0300

    style: add cairo font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import type { Metadata } from 'next'
 
-import localFont from 'next/font/local'
+import { Cairo } from 'next/font/google'
 import './globals.css'
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const cairo = Cairo({
+  weight: ['300', '400', '600', '700', '900'],
+  subsets: ['arabic'],
+  variable: '--font-cairo',
 })
 
 export const metadata: Metadata = {
@@ -27,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${cairo.variable} antialiased`}>{children}</body>
     </html>
   )
 }
